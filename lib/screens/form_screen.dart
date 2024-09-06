0d12800903348dff722eb269ea2b6db3162e4307
vitorLourival22 <vitorLou2204@gmail.com>
2024-09-06 19:36:50 -0300

    form update

---
+++ lib/screens/form_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class FormScreen extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Formulario'),
+    ),
+    body: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Image.network(
+              'https://via.placeholder.com/150',
+               width: 150,
+               height: 150,
+            ),
+            SizedBox(height: 20,),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'campo1'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'por favar,prencha este campo';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 20,),
+            ElevatedButton(
+              onPressed:() {
+                if (_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Processando dados')),
+                  );
+              }
+             },
+             child: Text('Enviar'),
+             ),
+             ],
+             ),
+             ),
+             ),
+             );
+             }
+             }
+            
